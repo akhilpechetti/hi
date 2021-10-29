f51603afbbd1eeaac98d05ca81db809b3e86eef4
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 00:24:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -33,5 +33,11 @@ def main():
         posts= api.user_timeline(screen_name='username',count=n,lang='en',tweet_mode='extended')
         st.write(posts[0:2])
 if __name__ == '__main__':
-    loading()
+    CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
+    CONSUMER_SECRET = '9I3BEaSP2LqS2wfQu0qXefJXDjUsqzouhoBvbDG6onv5VfU4lL'
+    OAUTH_TOKEN = '870901794452291584-zx8zAHDfvt9EdsCAAdNg9r5Se6GSiPP'
+    OAUTH_TOKEN_SECRET = 'RhOPigMTtITcw1c6O4L2wGg7qcgv7lqkzQpFpbFVyHM2e'
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+    api= tweepy.API(auth,wait_on_rate_limit=True)
     main()
