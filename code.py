2d171c91a8d88ad30544fb0d0182a6c230f85732
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-29 22:37:00 +0530

    Update code.py

--- code.py
+++ code.py
@@ -9,22 +9,21 @@ import joblib
 
 from PIL import Image
 def main():
-        CONSUMER_KEY = st.text_input('consumer key')
-        CONSUMER_SECRET = st.text_input('consumer secret')
-        OAUTH_TOKEN = st.text_input('oauth token')
-        OAUTH_TOKEN_SECRET = st.text_input('oauth token secret')
-        username = st.text_input('username want to check')
-        n=st.number_input('how many tweets',1,2000)
-        auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-        auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-        api= tweepy.API(auth,wait_on_rate_limit=True)
-        posts= api.user_timeline(screen_name=username,count=n,tweet_mode='extended')
-        st.success('logged in')
+    CONSUMER_KEY = st.text_input('consumer key')
+    CONSUMER_SECRET = st.text_input('consumer kry')
+    OAUTH_TOKEN = st.text_input('consumer kry')
+    OAUTH_TOKEN_SECRET = st.text_input('consumer kry')
+    username = st.text_input('username want to check')
+    n=st.number_input('how many tweets',1,2000)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+    api= tweepy.API(auth,wait_on_rate_limit=True)
+    posts= api.user_timeline(screen_name='username',count=n,lang='en',tweet_mode='extended')
+    if st.button['n recent tweets']:
         m=st.number_input('number',1,2000)
         i=1
-        if st.button('n recent tweets'):
-             for tweet in posts[0:m]:
-                print(str(i)+')' + tweet.full_text+'\n')
-                i=i+1
+        for tweet in posts[0:m]:
+            print(str(i)+')' + tweet.full_text+'\n')
+            i=i+1
  if __name__ == '__main__':
     main()
