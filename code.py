d97181cff44bb87e609553db072bef4e61c36083
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 00:23:13 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,6 +10,16 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 
 from PIL import Image
+def loading():
+    print('started')
+    CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
+    CONSUMER_SECRET = '9I3BEaSP2LqS2wfQu0qXefJXDjUsqzouhoBvbDG6onv5VfU4lL'
+    OAUTH_TOKEN = '870901794452291584-zx8zAHDfvt9EdsCAAdNg9r5Se6GSiPP'
+    OAUTH_TOKEN_SECRET = 'RhOPigMTtITcw1c6O4L2wGg7qcgv7lqkzQpFpbFVyHM2e'
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+    api= tweepy.API(auth,wait_on_rate_limit=True)
+    print('end')
 def main1():
     tweet=st.text_input('tweet')
     if st.button('1'):
@@ -17,17 +27,11 @@ def main1():
         st.write("{}… {}".format(tweet[:30], str(vs)))
 
 def main():
-    CONSUMER_KEY = st.text_area('consumer key')
-    CONSUMER_SECRET = st.text_area('sec')
-    OAUTH_TOKEN = st.text_area('ctok')
-    OAUTH_TOKEN_SECRET = st.text_area('tok sec')
     username = st.text_input('username want to check')
     n=st.number_input('how many tweets',1,2000)
     if st.button('login'):
-        auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-        auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-        api= tweepy.API(auth,wait_on_rate_limit=True)
         posts= api.user_timeline(screen_name='username',count=n,lang='en',tweet_mode='extended')
         st.write(posts[0:2])
 if __name__ == '__main__':
-        main()
+    loading()
+    main()
