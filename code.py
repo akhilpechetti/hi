444cb5c6c3e1bfac9e64a7bc37684eb9994c7de3
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 00:53:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
     api= tweepy.API(auth,wait_on_rate_limit=True)
     tweets=[]
-    for i in tweepy.Cursor(api.user_search,q='vaccine').items(3):
+    for i in tweepy.Cursor(api.search,q='vaccine').items(3):
         tweets.append(i.text)
     print(tweets)
     #cursor=tweepy.Cursor(api.user_timeline,id=user).items(10)
