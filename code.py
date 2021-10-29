753bb6845e8204838dd229642fb82dcc66cc9e22
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-29 22:18:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -16,18 +16,18 @@ def main():
     OAUTH_TOKEN_SECRET = st.text_input('oauth token secret')
     username = st.text_input('username want to check')
     n=st.number_input('how many tweets',1,2000)
-    if st.button('login'):
+    for st.button('login'):
         global posts
         auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
         auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
         api= tweepy.API(auth,wait_on_rate_limit=True)
         posts= api.user_timeline(screen_name=username,count=n,tweet_mode='extended')
         st.success('logged in')
-        st.button('n recent tweets')
-            m=st.number_input('number',1,2000)
-            i=1
-            for tweet in posts[0:m]:
-                print(str(i)+')' + tweet.full_text+'\n')
+        if st.button('n recent tweets'):
+                m=st.number_input('number',1,2000)
+                i=1
+                for tweet in posts[0:m]:
+                    print(str(i)+')' + tweet.full_text+'\n')
                     i=i+1
 if __name__ == '__main__':
     main()
