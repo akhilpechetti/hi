bd36cbb27dcfd1427345fe51f2f7e4c1546114cd
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 08:18:08 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,12 +6,12 @@ import tweepy
 #from flasgger import Swagger
 import streamlit as st
 import joblib
+#import re
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 
 from PIL import Image
-def main():
-    import tweepy
+def creat():
     CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
     CONSUMER_SECRET = '9I3BEaSP2LqS2wfQu0qXefJXDjUsqzouhoBvbDG6onv5VfU4lL'
     OAUTH_TOKEN = '870901794452291584-zx8zAHDfvt9EdsCAAdNg9r5Se6GSiPP'
@@ -29,6 +29,10 @@ def main():
         st.write(str(i)+')' + tweet.full_text+'\n')
         i=i+1
     st.dataframe(df)
+def main():
+    import tweepy
+    creat()
+    
 if __name__ == '__main__':
     #cursor=tweepy.Cursor(api.user_timeline,id=user).items(10)
     main()
