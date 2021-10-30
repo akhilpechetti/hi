03f520c656f4f5966ab85ded50eb095f126b9934
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 08:03:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,53 +10,6 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 
 from PIL import Image
-def loading():
-    print('started')
-    CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
-    CONSUMER_SECRET = '9I3BEaSP2LqS2wfQu0qXefJXDjUsqzouhoBvbDG6onv5VfU4lL'
-    OAUTH_TOKEN = '870901794452291584-zx8zAHDfvt9EdsCAAdNg9r5Se6GSiPP'
-    OAUTH_TOKEN_SECRET = 'RhOPigMTtITcw1c6O4L2wGg7qcgv7lqkzQpFpbFVyHM2e'
-    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-    auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-    api= tweepy.API(auth,wait_on_rate_limit=True)
-    print('end')
-def main1():
-    tweet=st.text_input('tweet')
-    if st.button('1'):
-        vs = analyzer.polarity_scores(tweet)
-        st.write("{}… {}".format(tweet[:30], str(vs)))
-
-def main2():
-    st.title('file upload')
-    menu=['dataset']
-    choice=st.sidebar.selectbox('menu',menu)
-    if choice=='dataset':
-        st.subheader('dataset')
-def main112():
-    from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-    analyzer = SentimentIntensityAnalyzer()
-    #analyzer = SentimentIntensityAnalyzer()
-    import streamlit as st
-    s1=st.text_input('tweet')
-    vs = analyzer.polarity_scores(s1)
-    st.write("{}… {}".format(s1[:30], str(vs)))
-    import twitter
-    CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
-    CONSUMER_SECRET = '9I3BEaSP2LqS2wfQu0qXefJXDjUsqzouhoBvbDG6onv5VfU4lL'
-    OAUTH_TOKEN = '870901794452291584-zx8zAHDfvt9EdsCAAdNg9r5Se6GSiPP'
-    OAUTH_TOKEN_SECRET = 'RhOPigMTtITcw1c6O4L2wGg7qcgv7lqkzQpFpbFVyHM2e'
-    auth = twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
-    twitter_api = twitter.Twitter(auth=auth)
-    count = 20
-    query = st.text_input('query')
-    if st.button('search'):
-        tweets = twitter_api.search.tweets(q=query, count=count,lang='en',tweet_mode="extended")
-        tweetsWithSent = []
-        for t in tweets['statuses']:
-           text = (t['full_text'])
-           ps = analyzer.polarity_scores(text)
-           tweetsWithSent.append({'text':text, 'compound':ps['compound']})
-        st.write(tweetsWithSent)
 def main():
     import tweepy
     CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
