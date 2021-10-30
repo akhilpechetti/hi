8097d429f8c3f48e3b59f801ba097c2760f3dcf7
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 08:50:19 +0530

    Update code.py

--- code.py
+++ code.py
@@ -64,7 +64,7 @@ def wordcl():
     wordcloud=WordCloud(width=500, height=300,random_state=21,max_font_size=119).generate(allwords)
     plt.imshow(wordcloud,interpolation='bilinear')
     plt.axis('off')
-    plt.show()
+    st.image(plt.show())
 def main():
     import tweepy
     creat()
