45abd5bf0daa2b1ee0bee1b41972f85d5760751b
akhilpechetti <87658263+akhilpechetti@users.noreply.github.com>
2021-10-30 06:57:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -27,26 +27,11 @@ def main1():
         st.write("{}… {}".format(tweet[:30], str(vs)))
 
 def main():
-    user = st.text_input('username want to check')
-    n=st.number_input('how many tweets',1,2000)
-    if st.button('login'):
-        tweets=[]
-        for i in tweepy.Cursor(api.user_timeline,id=user).items(n):
-            tweets.append(i.text)
-        str.success(tweets)
-        #posts= api.user_timeline(screen_name='username',count=n,lang='en',tweet_mode='extended')
-        #st.write(posts[0:2])
+    st.title('file upload')
+    menu=['dataset']
+    choice=st.sidebar.selectbox('menu',menu)
+    if choice=='dataset':
+        st.subheader('dataset')
 if __name__ == '__main__':
-    CONSUMER_KEY = '3tDVUGFlwUAfrjtTNO6k1xfqW'
-    CONSUMER_SECRET = '9I3BEaSP2LqS2wfQu0qXefJXDjUsqzouhoBvbDG6onv5VfU4lL'
-    OAUTH_TOKEN = '870901794452291584-zx8zAHDfvt9EdsCAAdNg9r5Se6GSiPP'
-    OAUTH_TOKEN_SECRET = 'RhOPigMTtITcw1c6O4L2wGg7qcgv7lqkzQpFpbFVyHM2e'
-    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-    auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-    api= tweepy.API(auth,wait_on_rate_limit=True)
-    tweets=[]
-    for i in tweepy.Cursor(api.search,q='vaccine').items(3):
-        tweets.append(i.text)
-    print(tweets)
     #cursor=tweepy.Cursor(api.user_timeline,id=user).items(10)
-    #main()
+    main()
